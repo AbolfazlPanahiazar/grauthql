d07d8be5ae0aea8cd4f33e00ccaec36b38df8a29
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-12-08 19:55:23 +0330

    Add Header component

--- client/components/Header.js
+++ client/components/Header.js
@@ -4,9 +4,23 @@ import { graphql } from "react-apollo";
 import query from "../queries/CurrentUser";
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+    if (loading) return <div />;
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return <div>you're not signed in.</div>;
+    }
+  }
+
   render() {
     console.log(this.props.data);
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">{this.renderButtons()}</div>
+      </nav>
+    );
   }
 }
 
