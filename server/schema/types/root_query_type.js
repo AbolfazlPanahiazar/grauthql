be992ff9f4749b5ed1dfaf8ccc4732fab232c01d
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-12-08 04:51:36 +0330

    Add user to authenticated query

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,17 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLID } = graphql;
 
+const UserType = require("./user_type");
+
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    testField: { type: GraphQLID },
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      }
+    }
   },
 });
 
