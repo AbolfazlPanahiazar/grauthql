71985cd832b7c0c573f7e51d7257d7d273f71d25
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-12-08 04:38:27 +0330

    Add login and logout mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -13,9 +13,26 @@ const mutation = new GraphQLObjectType({
         email: { type: GraphQLString },
         password: { type: GraphQLString },
       },
-      // Second one is args and third is context or request
       resolve(parentValue, { email, password }, req) {
         return AuthService.signup({ email, password, req });
+      },
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      },
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req });
       },
     },
   },
