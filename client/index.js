c689a47330f4ad9f5d3cee48216c2ed47a11f1cc
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-12-09 05:22:35 +0330

    Add sign up page and mutation

--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ import { Router, Route, hashHistory } from "react-router";
 
 import App from "./components/App";
 import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
 
 const networkInterface = createNetworkInterface({
   uri: "/graphql",
@@ -25,6 +26,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <Route path="/login" component={LoginForm} />
+          <Route path="/signup" component={SignupForm} />
         </Route>
       </Router>
     </ApolloProvider>
